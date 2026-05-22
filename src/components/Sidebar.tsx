69075ab45b94ddc4e144c7fb1f51968595e23b8c
Amin <amin562amin@gmail.com>
2026-05-22 15:16:03 +0100

    Implemented styling tweaks to SideBar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,18 +5,19 @@ function Sidebar()
     const[pressed, setPressed] = useState(false);
 
     const globalStyling = "absolute top-0 left-0 h-screen w-20 bg-[#12171e] px-3 py-2 transition-all duration-500 ease-in-out m-0 p-0 box-border font-serif"
+    const textStyling = "text-white flex items-center no-underline rounded-xl"
     return(
-        <aside className={`${globalStyling} ${pressed ? "w-64" : "w-20"}`}>
+        <aside className={` ${globalStyling} ${pressed ? "w-45" : "w-20"}`}>
            
-       <div className="logo">
+       <div className={`logo ${textStyling}`}>
         <button onClick={() => setPressed(!pressed)}>             
             <Menu className='btn'/>  
         </button>
        </div> 
-       <ul>
+       <ul className='space-y-4'>
         <li>
             <a href="#" 
-            className="flex items-center gap-3">
+            className={`flex items-center gap-3 ${textStyling}`}>
                 <PenAlt className='text-2xl' />
                 {pressed && <span> Dashboard </span>}
             </a>
@@ -25,7 +26,7 @@ function Sidebar()
 
         <li>
             <a href="#"
-            className="flex items-center gap-3">
+            className={`flex items-center gap-3 ${textStyling}`}>
                 <PenAlt className='text-2xl' />
                 {pressed && <span>Modules </span>}
             </a>
@@ -33,7 +34,7 @@ function Sidebar()
         </li>
         <li>
             <a href="#"
-            className="flex items-center gap-3">
+            className={`flex items-center gap-3 ${textStyling}`}>
                 <ChartSine className='text-2xl' />
                 {pressed &&<span>Analytics </span>}
             </a>
@@ -41,7 +42,7 @@ function Sidebar()
         </li>
         <li>
             <a href="#"
-            className="flex items-center gap-3">
+            className={`flex items-center gap-3 ${textStyling}`}>
                 <Cog className='text-2xl' />
                 {pressed && <span>Settings </span>}
             </a>
